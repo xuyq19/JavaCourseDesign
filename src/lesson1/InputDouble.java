f1b79cdc5e8c812aad707fd5f14cafc2065efcd7
Lucas Xu <xuyuqiabcd@outlook.com>
2022-02-26 14:20:41 +0800

    creat

---
+++ src/lesson1/InputDouble.java
@@ -0,0 +1,14 @@
+package lesson1;
+import java.util.*;
+public class InputDouble {
+    public static void main(String[] args){
+        double a,b,c;
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a float number from the keyboard: ");
+        a = reader.nextDouble();
+        b=a*a;
+        c=a*a*a;
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
---
+++ src/lesson1/paramtest.java
@@ -0,0 +1,8 @@
+package lesson1;
+
+public class paramtest {
+    public static void main(String args[]) {
+        System.out.println("\n第一个参数是：" + args[0]);
+        System.out.println("\n第二个参数是：" + args[1]);
+    }
+}
